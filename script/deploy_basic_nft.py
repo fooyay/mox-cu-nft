f769a69f7d24a78674f5551b47f46758926a973f
Sean Coates <sean.coates+fooyay@gmail.com>
2026-02-06 09:23:06 -0500

    change NFT image to a monkey

--- script/deploy_basic_nft.py
+++ script/deploy_basic_nft.py
@@ -1,13 +1,17 @@
 from src import basic_nft
 
+# This points to a full JSON metadata file stored on IPFS
 PUG_URI = "QmW16U98JrY9HBY36rQtUuUtDnm6LdEeNdAAggmrx3thMa"
+
+# These point to just the image file stored on IPFS
 CENTRALIZED_URI = "QmanoFs6a4GHRtTW32w1fFJSv61TFT8Vk9qKqFF1mcdjMK"
+MONKEY_URI = "QmaKJVC9cCRCK39FBJ5t5v52FqZAdzLV7xqS1LfJ4NuiUu"
 
 
 def deploy_basic_nft():
     contract = basic_nft.deploy()
     # contract.mint(PUG_URI)
-    contract.mint(CENTRALIZED_URI)
+    contract.mint(MONKEY_URI)
     token_uri = contract.tokenURI(0)
     print(f"Basic NFT deployed to {contract.address}")
     print(f"Token URI: {token_uri}")
